86d2005921bc89e4d05496311881e65146788e63
ak18dec <ankitdayal1992@gmail.com>
2026-07-20 18:02:33 +0530

    Schema Updated

--- services/fleet-service/src/main/resources/schema.sql
+++ services/fleet-service/src/main/resources/schema.sql
@@ -1,17 +1,16 @@
 CREATE TABLE IF NOT EXISTS maintenance_requests(
-    id,
-    vehicle_id,
-    description,
-    status,
-    provider_id,
-    assigned_at,
-    findings,
-    estimated_cost,
-    estimate_submitted_at,
-    decision_note,
-    decided_at,
-    created_at,
-    updated_at,
+    id                          UUID                        NOT NULL,
+    vehicle_id                  VARCHAR(255)                NOT NULL,
+    description                 VARCHAR(1000)               NOT NULL,
+    status                      VARCHAR(50)                 NOT NULL,
+    provider_id                 VARCHAR(255),
+    assigned_at                 TIMESTAMP WITH TIME ZONE,
+    findings                    VARCHAR(2000),
+    estimated_cost              DOUBLE PRECISION,
+    estimate_submitted_at       TIMESTAMP WITH TIME ZONE,
+    decision_note               VARCHAR(1000),
+    decided_at                  TIMESTAMP WITH TIME ZONE,
+    created_at                  TIMESTAMP WITH TIME ZONE    NOT NULL,
+    updated_at                  TIMESTAMP WITH TIME ZONE    NOT NULL,
     PRIMARY KEY (id)
-
-)
+);
